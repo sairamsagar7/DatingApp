d672e9f051dbd26112403f1ecf8fcfdd2ef0b3a6
Lakshman samanth <sairamsagar7@gmail.com>
2020-11-04 12:19:34 +0530

    Initial Commit

---
+++ DatingApp-SPA/src/app/Value/Value.component.ts
@@ -0,0 +1,26 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+
+
+@Component({
+  // tslint:disable-next-line: component-selector
+  selector: 'app-Value',
+  templateUrl: './Value.component.html',
+  styleUrls: ['./Value.component.css']
+})
+export class ValueComponent implements OnInit {
+values: any;
+  constructor(private http: HttpClient) { }
+
+  ngOnInit() {
+    this.getValues();
+  }
+  getValues(){
+    this.http.get('http://localhost:5000/api/values').subscribe(response => {
+      this.values = response;
+    }, error => {
+      console.log(error);
+    });
+  }
+
+}
